bab13ed15e227826a21c873eb986a85efa77a945
Atsighi Bright <brytebee@gmail.com>
2022-09-30 11:03:34 +0100

    :white_check_mark: Update Home.jsx
    Added content and functional button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+import landing from "../assets/landing.svg";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <section>
+        <p>Imagine if</p>
+        <p>Snapchat</p>
+        <p>had events.</p>
+        <div>
+          <p>
+            Easily host and share events with your friends across any social
+            media.
+          </p>
+        </div>
+      </section>
+      <img src={landing} alt="Lannding page" />
+      <NavLink to="/create">
+        <button type="button">Create my event</button>
+      </NavLink>
+    </div>
+  );
 };
 
 export default Home;
