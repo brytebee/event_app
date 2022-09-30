422d6cb7526369d61d5dbdbca333d2857b21d956
Atsighi Bright <brytebee@gmail.com>
2022-09-30 10:44:56 +0100

    :heavy_plus_sign: Update codebase
    Added components and Navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import Form from "./components/Form";
+import Event from "./components/Event";
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+
 function App() {
-  return <div className="App">Hello world!</div>;
+  return (
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/create" element={<Form />} />
+        <Route exact path="/event" element={<Event />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
